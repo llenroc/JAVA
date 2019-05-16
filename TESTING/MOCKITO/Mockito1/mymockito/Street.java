e023a557414cf04a581b01ec2633e6cbafd7f397
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2019-05-16 00:11:51 -0500

    Create Street.java

---
+++ TESTING/MOCKITO/Mockito1/mymockito/Street.java
@@ -0,0 +1,16 @@
+package mymockito;
+
+public class Street {
+
+    private String building;
+
+    public Street(String building) {
+        this.building = building;
+    }
+
+    public String getBuilding() {
+		return building;
+	}
+
+
+}
