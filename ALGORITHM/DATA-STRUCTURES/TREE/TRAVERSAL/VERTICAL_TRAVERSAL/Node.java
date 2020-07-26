76b9f5d09a701a43427cd5d0d4b01e6db727b937
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2020-07-26 00:38:24 -0500

    Create Node.java

---
+++ ALGORITHM/DATA-STRUCTURES/TREE/TRAVERSAL/VERTICAL_TRAVERSAL/Node.java
@@ -0,0 +1,17 @@
+public class Node {
+	private int value;
+	Node left;
+	Node right;
+
+	public Node(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void displayValue() {
+		System.out.print(value + " ");
+	}
+}
