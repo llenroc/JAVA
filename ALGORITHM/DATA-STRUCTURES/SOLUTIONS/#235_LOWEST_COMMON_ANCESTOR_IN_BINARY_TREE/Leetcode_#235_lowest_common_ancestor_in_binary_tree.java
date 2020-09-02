e30a88430aa59b252b99975fdd239e6ff2a85b11
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2020-09-02 15:32:46 -0500

    Create Leetcode_#235_lowest_common_ancestor_in_binary_tree.java

---
+++ ALGORITHM/DATA-STRUCTURES/SOLUTIONS/#235_LOWEST_COMMON_ANCESTOR_IN_BINARY_TREE/Leetcode_#235_lowest_common_ancestor_in_binary_tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/*
+2020-Sep-02. Oleksii Saiun.
+LeetCode_#235. Lowest Common Ancestor of a Binary Tree
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+*/
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(root==null)
+        {
+            return null;
+        }
+        
+        if (root.val == p.val)
+        {
+            return root;
+        }
+        
+        if (root.val == q.val)
+        {
+            return root;
+        }
+        
+        TreeNode leftRoot = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightRoot = lowestCommonAncestor(root.right, p, q);
+        
+        if (leftRoot !=null  && rightRoot !=null)
+        {
+            return root;
+        }
+        else if (leftRoot!=null && rightRoot==null)
+        {
+            return leftRoot;
+        }
+        else if (leftRoot==null && rightRoot!=null)
+        {
+            return rightRoot;
+        }           
+        else
+        {
+            return null;
+        }
+    }
+}
