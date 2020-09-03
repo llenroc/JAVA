e04f80e5989b5691c8d934d0388bc9451eba515c
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2020-09-03 00:36:33 -0500

    Create Node.java

---
+++ ALGORITHM/DATA-STRUCTURES/TREE/SEARCH_IN_BINARY_SEARCH_TREE/Node.java
@@ -0,0 +1,18 @@
+public class Node { 
+	private int value;
+	Node left;
+	Node right;
+
+	
+	public Node(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void displayValue() {
+		System.out.print(value + " ");
+	}
+} 
