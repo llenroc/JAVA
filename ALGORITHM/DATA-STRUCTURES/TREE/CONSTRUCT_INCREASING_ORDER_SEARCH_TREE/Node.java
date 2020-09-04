ecb23d8f64850b07f54a0f159b0f05a6422963c7
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2020-09-03 20:14:20 -0500

    Create Node.java

---
+++ ALGORITHM/DATA-STRUCTURES/TREE/CONSTRUCT_INCREASING_ORDER_SEARCH_TREE/Node.java
@@ -0,0 +1,19 @@
+public class Node {
+	int value;
+	Node left;
+	Node right;
+    
+	public Node(int value)
+	{
+		this.value=value;
+	}
+	public int getValue()
+	{
+		return value;
+	}
+	
+	public void displayValue()
+	{
+		System.out.print(value+" ");
+	}
+}
