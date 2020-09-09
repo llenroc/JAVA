6eeb400d90453788bcce48dd8b4f70142329ac6a
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2020-09-09 00:01:32 -0500

    Create Leetcode_#543_DiameterofBinaryTree.java

---
+++ ALGORITHM/DATA-STRUCTURES/SOLUTIONS/LEETCODE/#543_DIAMETER_OF_BINARY_TREE/Leetcode_#543_DiameterofBinaryTree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root==null)
+        {
+            return 0;
+        }
+
+        int diameterViaRoot = calcHeight(root.left) + calcHeight(root.right);
+        int diameterNotViaRoot = Math.max(calcHeight(root.left), calcHeight(root.right));
+        int diameter = Math.max(diameterViaRoot,diameterNotViaRoot);  
+        return diameter;
+    }
+    
+    
+        public int calcHeight(TreeNode root) {
+        
+        if (root==null)
+        {
+            return 0;
+        }
+        
+        int leftDepth = calcHeight(root.left);
+        int rightDepth = calcHeight(root.right);
+        
+       if ()
+       {
+           
+       }
+    }
+}
