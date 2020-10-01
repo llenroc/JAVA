2e7cc070046f8a7b6d173a872d22bc3133bec8f4
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2020-10-01 18:48:38 -0500

    Create Leetcode_#082_Sales_Analysis_I.sql

---
+++ ALGORITHM/DATA-STRUCTURES/SOLUTIONS/LEETCODE/SQL/#082_SALES_ANALYSIS_I/Leetcode_#082_Sales_Analysis_I.sql
@@ -0,0 +1,18 @@
+/* Write your PL/SQL query statement below */
+/*
+2020-Oct-01. Oleksii Saiun.
+LeetCode_#1082. Sales Analysis I
+https://leetcode.com/problems/sales-analysis-i/
+*/
+SELECT SELLER_ID 
+FROM
+(
+SELECT RANK() OVER(ORDER BY TOTAL DESC) AS RN,SELLER_ID,TOTAL
+FROM(
+     SELECT SELLER_ID, SUM(PRICE) AS TOTAL
+     FROM SALES
+     GROUP BY SELLER_ID
+    )
+    T1
+)T2
+WHERE RN=1;
