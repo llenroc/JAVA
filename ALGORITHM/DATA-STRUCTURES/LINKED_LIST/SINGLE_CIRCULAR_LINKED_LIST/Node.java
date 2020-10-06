3150a59bc71051a6efa42c1f1a6afc1b2b72d2e9
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2020-10-06 14:47:50 -0500

    Create Node.java

---
+++ ALGORITHM/DATA-STRUCTURES/LINKED_LIST/SINGLE_CIRCULAR_LINKED_LIST/Node.java
@@ -0,0 +1,15 @@
+public class Node {
+	private int value;
+	Node next;
+
+	public Node(int value) {
+		super();
+		this.value = value;
+	}
+	
+	public void displayValue()
+	{
+		System.out.print(value+" ");
+	}
+
+}
