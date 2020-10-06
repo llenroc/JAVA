b5c52dbe142a3c208eb3b3c007a5e224cd0b0884
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2020-10-06 14:35:17 -0500

    Update TreeConvertToDoublyLinkedListApp.java

--- ALGORITHM/DATA-STRUCTURES/TREE/CONVERT_LINKED_LIST_TO_DOUBLY_LINKED_LIST/TreeConvertToDoublyLinkedListApp.java
+++ ALGORITHM/DATA-STRUCTURES/TREE/CONVERT_LINKED_LIST_TO_DOUBLY_LINKED_LIST/TreeConvertToDoublyLinkedListApp.java
@@ -2,61 +2,47 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 
-/*2020-Sep-27. Oleksii Saiun. 
- * Convert Binary Tree to Doubly LinkedList in [in-order] mode
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
- * */
 class Tree {
 
-	Node head;
-	Node tail;
+	Stack<Integer> stack = new Stack<>();
 
-	Node nodeLinked=null;
-    public void inOrder(Node root)
-    {
+	public void inOrder(Node root) {
 		if (root == null) {
 			return;
 		}
-		
 		inOrder(root.left);
-		if(head==null)
-		{
-			Node newNode = new Node(root.getValue());
-			head=newNode;
-			tail=newNode;
-		}
-		else
-		{
-			Node newNode = new Node(root.getValue());
-			head.right=newNode;
-			newNode.left=head;
-			head=newNode;
-		}
+		stack.push(root.getValue());
 		inOrder(root.right);
-    }
+	}
 
-    public void diplayAllLinkedListMoveToLeft()
-    {
-    	Node curr= head;
-    	while(curr!=null)
-    	{
-    		curr.displayValue();
-    		curr=curr.left;
-    	}
-    	System.out.println();
-    }
-    
-    public void diplayAllLinkedListMoveToRight()
-    {
-    	Node curr= tail;
-    	while(curr!=null)
-    	{
-    		curr.displayValue();
-    		curr=curr.right;
-    	}
-    	System.out.println();
-    }
-    
+	public Node convert(Node root) {
+		inOrder(root);
+
+		Node newNode = new Node(stack.pop());
+
+		Node head = newNode;
+		Node tail = newNode;
+		head.left=tail;
+		tail.right=head;
+		
+		while (!stack.isEmpty()) {
+
+			newNode = new Node(stack.pop());
+
+			newNode.right = head;
+			head.left = newNode;
+			head = newNode;
+			head.left = tail;
+			tail.right = head;
+
+		}
+		return head;
+	}
+
 	public void BFS(Node root) {
 		if (root == null) {
 			return;
@@ -72,64 +58,53 @@ class Tree {
 			if (tmpNode.left != null) {
 				queue.offer(tmpNode.left);
 			}
-
 			if (tmpNode.right != null) {
 				queue.offer(tmpNode.right);
 			}
 		}
 		System.out.println();
 	}
-
 }
 
 public class TreeConvertToDoublyLinkedListApp {
-
 	public static void main(String[] args) {
-		Node root = new Node(1);
+		Tree tree = new Tree();
+		Node root = new Node(4);
+		Node node1 = new Node(1);
 		Node node2 = new Node(2);
 		Node node3 = new Node(3);
-		Node node4 = new Node(4);
 		Node node5 = new Node(5);
-		Node node6 = new Node(6);
-		
-		root.left=node2;
-		root.right=node5;
-		node2.left=node3;
-		node2.right=node4;
-		node5.right=node6;
 
-		Tree tree = new Tree();
+		root.left = node2;
+		root.right = node5;
+		node2.left = node1;
+		node2.right = node3;
 
 		tree.BFS(root);
-		tree.inOrder(root);
-		System.out.println("Print Linked list - move to left:");
-		tree.diplayAllLinkedListMoveToLeft();
-		System.out.println("Print Linked list - move to right:");
-		tree.diplayAllLinkedListMoveToRight();
-		
+
+		Node out = tree.convert(root);
+		out.displayValue();
+		System.out.println();
+		out.right.displayValue();
+	    System.out.println();
+		out.right.right.displayValue();
+		System.out.println();
+		out.right.right.right.displayValue();
+		System.out.println();
+		out.right.right.right.right.displayValue();
+		System.out.println();
+		System.out.println("moves a cycle:");
+		out.right.right.right.right.right.displayValue();
 
 
-
-/*
-    1
-   / \
-  2   5
- / \   \
-3   4   6   
-
-Level Order:
-1 2 5 3 4 6 
-Print Linked list - move to left:
-6 5 1 4 2 3 
-Print Linked list - move to right:
-3 2 4 1 5 6 
-
-
-
-*/
-		
+		/*
+		 *               4
+		 *             /   \
+		 *            2     5
+		 *          /  \
+		 *         1    3
+		 * 
+		 */	
 	}
 
 }
-
-
