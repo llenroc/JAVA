f4644982a03a87e04e6d6934ac244333cbc74a08
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2020-10-19 17:21:00 -0500

    Update SmallestSubarrayWithGivenSumApp.java

--- ALGORITHM/DATA-STRUCTURES/PATTERN_SLIDING_WINDOW/SMALLEST_SUBARRAY_WITH_GIVEN_SUM/SmallestSubarrayWithGivenSumApp.java
+++ ALGORITHM/DATA-STRUCTURES/PATTERN_SLIDING_WINDOW/SMALLEST_SUBARRAY_WITH_GIVEN_SUM/SmallestSubarrayWithGivenSumApp.java
@@ -1,39 +1,42 @@
-import static org.junit.Assert.assertEquals;
+/*
+ * 2020-Oct-19. Oleksii Saiun. Smallest Subarray with a given sum 
+ * Sliding window pattern
+ * 
+ * arr={ 2, 1, 5, 2, 3, 2 };
+ * sum=7;
+ * result: 2;
+ * */
 
-import org.junit.jupiter.api.Test;
+class Calc {
+	public int findMinSubArray(int[] arr, int s) {
+		int startWindow = 0;
+		int endWindow = 0;
+		int sum = 0;
+		int minLength = Integer.MAX_VALUE;
+		boolean flagSumExists=false;
 
-public class TestSmallestSubarrayWithGivenSumApp {
-	private Calc calc = new Calc();
+		for (endWindow = 0; endWindow < arr.length; endWindow++) {
+			sum = sum + arr[endWindow];
 
-	@Test
-	public void testFindMinSubArray1() {
-		int[] arr = new int[] { 2, 1, 5, 2, 3, 2 };
-		int s = 7;
-		int out = 2;		
-		assertEquals(calc.findMinSubArray(arr, s),out);
-	}
-	
-	@Test
-	public void testFindMinSubArray2() {
-		int[] arr = new int[] { 2, 1, 5, 2, 8 };
-		int s = 7;
-		int out = 1;		
-		assertEquals(calc.findMinSubArray(arr, s),out);
-	}
-	
-	@Test
-	public void testFindMinSubArray3() {
-		int[] arr = new int[] { 3, 4, 1, 1, 6 };
-		int s = 8;
-		int out = 3;		
-		assertEquals(calc.findMinSubArray(arr, s),out);
-	}
-	
-	@Test
-	public void testFindMinSubArrayZero() {
-		int[] arr = new int[] { 3, 4, 1, 1, 6 };
-		int s = 180;
-		int out = 0;		
-		assertEquals(calc.findMinSubArray(arr, s),out);
+			while (sum >= s) {
+				int lengthOfSubarray = endWindow - startWindow + 1;
+				minLength = Math.min(minLength, lengthOfSubarray);
+				System.out.println("startWindow:" + startWindow + "; endWindow:" + endWindow + "; sum:" + sum
+						+ "; minLength: " + minLength);
+				sum = sum - arr[startWindow++];
+				
+				flagSumExists=true;//the tasks require if you can find sum [s] than rturn [0]. This flag is just a marker that there is a sum
+			}
+		}
+
+		if(flagSumExists)
+		{
+			return minLength;
+		}
+		else
+		{
+			return 0;
+		}
+		
 	}
 }
