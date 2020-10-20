74284de76baabb8f5252184a8ac530665e4ecf07
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2020-10-20 18:00:24 -0500

    Create Employee.java

---
+++ CODE/SORTING/SORT_COMPARABLE/Employee.java
@@ -0,0 +1,83 @@
+public class Employee implements Comparable<Employee>{
+	private String firstName;
+	private String lastName;
+	private int age;
+	private int salary;
+	public Employee(String firstName, String lastName, int age, int salary) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "[firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", salary=" + salary
+				+ "]";
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	@Override
+	public int compareTo(Employee e) {        
+		return this.firstName.compareTo(e.firstName);
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + salary;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (age != other.age)
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (salary != other.salary)
+			return false;
+		return true;
+	}
+	
+	
+}
