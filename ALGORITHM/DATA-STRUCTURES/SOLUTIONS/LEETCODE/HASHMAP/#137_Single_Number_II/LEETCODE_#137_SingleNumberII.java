5f83e84eea99b1bb0754fdabba6b948ff930e1c8
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2021-01-19 16:41:17 +0200

    Create LEETCODE_#137_SingleNumberII.java

---
+++ ALGORITHM/DATA-STRUCTURES/SOLUTIONS/LEETCODE/HASHMAP/#137_Single_Number_II/LEETCODE_#137_SingleNumberII.java
@@ -0,0 +1,36 @@
+/*
+2021-Jan-19. Oleksii Saiun.
+LeetCode_137. Single Number II
+https://leetcode.com/problems/single-number-ii/
+*/
+
+class Solution {
+    private HashMap<Integer, Integer> map = new HashMap<>();
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>(
+            (o1,o2) -> map.get(o1) -  map.get(o2)
+    );
+    
+    public int singleNumber(int[] nums) {
+        populateHashMap(nums);
+        
+        for(Map.Entry<Integer, Integer>  j : map.entrySet())
+        {
+            if( j.getValue() == 1)
+            {
+                return j.getKey();
+            }
+        }
+        
+        return -1;
+    }
+    
+    private void populateHashMap(int[] nums)
+    {
+        for(int j=0; j<nums.length; j++)
+        {
+            int key = nums[j];
+            int currFreq = map.getOrDefault(key, 0);
+            map.put(key, currFreq+1);
+        }
+    }
+}
