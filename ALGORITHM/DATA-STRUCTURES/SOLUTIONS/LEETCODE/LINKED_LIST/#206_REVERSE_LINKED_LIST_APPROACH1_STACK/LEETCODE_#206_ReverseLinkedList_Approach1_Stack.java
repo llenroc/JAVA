46d3d4c5d9cf1451942b03bc32e3242ea284219d
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2021-02-15 23:28:59 -0600

    Create LEETCODE_#206_ReverseLinkedList_Approach1_Stack.java

---
+++ ALGORITHM/DATA-STRUCTURES/SOLUTIONS/LEETCODE/LINKED_LIST/#206_REVERSE_LINKED_LIST_APPROACH1_STACK/LEETCODE_#206_ReverseLinkedList_Approach1_Stack.java
@@ -0,0 +1,47 @@
+/*
+2021-Feb-15. Oleksii Saiun.
+LeetCode_#206. Reverse Linked List.
+https://leetcode.com/problems/reverse-linked-list/
+
+Approach1. Stack
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        Stack<Integer> stack = populateStack(head);
+        ListNode dummy = new ListNode(-1);
+        head=dummy;
+
+        while(!stack.isEmpty())
+        {
+           ListNode newNode = new ListNode(stack.pop());
+           head.next=newNode;
+           head=newNode;
+          
+        }
+        return dummy.next;
+    }
+    
+    private Stack<Integer> populateStack(ListNode head)
+    {
+      Stack<Integer> stackOut = new Stack<>();
+        
+      while(head!=null)
+      {
+          stackOut.add(head.val);
+          head=head.next;
+      }
+        
+        return stackOut;
+    }
+}
