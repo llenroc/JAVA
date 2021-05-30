8b46b13d5c12d92bef90c0bb564da8841d1477f7
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2021-05-30 18:28:23 -0500

    Create LEETCODE_#1710_MaximumUnitsOnTruck.java

---
+++ ALGORITHM/DATA-STRUCTURES/SOLUTIONS/LEETCODE/HEAPS/#1710_MAXIMUM_UNITS_ON_TRUCK/LEETCODE_#1710_MaximumUnitsOnTruck.java
@@ -0,0 +1,66 @@
+/*
+2021-May-30. Oleksii Saiun.
+LeetCode_#1710. Maximum Units on a Truck
+https://leetcode.com/problems/maximum-units-on-a-truck/
+
+Return the maximum total number of units that can be put on the truck.
+Input: boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
+Output: 8
+
+Solution:
+The total number of units will be = (1 * 3) + (2 * 2) + (1 * 1) = 8.
+
+*/
+
+
+class Solution {
+    /*
+    Time complexity: O(N*lgN)
+    Space complexity: O(1)
+    */
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        PriorityQueue<int[]> maxHeap = buildMaxHeap(boxTypes);
+        int remainingSize=truckSize;
+        int totalOfUnits=0;
+
+        while(!maxHeap.isEmpty())
+        {
+            if(remainingSize==0)
+            {
+                return totalOfUnits;
+            }
+            int[] currValue = maxHeap.poll();
+            int currNumberOfBoxes=currValue[0];
+            int currNumberOfUnits=currValue[1];
+            if(remainingSize>=currNumberOfBoxes)
+            {
+                remainingSize=remainingSize-currNumberOfBoxes;
+                int currAmount = currNumberOfBoxes*currNumberOfUnits;
+                totalOfUnits=totalOfUnits + currAmount; 
+            }
+            else
+            {
+
+                int currAmount = remainingSize*currNumberOfUnits;
+                totalOfUnits=totalOfUnits + currAmount;                 
+                remainingSize=0;
+            }
+            
+        }
+        return totalOfUnits;
+    }
+    
+    private PriorityQueue<int[]> buildMaxHeap(int[][] boxTypes)
+    {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>( 
+                                                             (o1,o2) -> o2[1]-o1[1]
+                                                            );    
+        for(int j=0;j<boxTypes.length;j++)
+        {
+            int[] currValue = new int[]{boxTypes[j][0],boxTypes[j][1]};
+            maxHeap.add(currValue);
+        }
+        
+        return maxHeap;
+    }
+}
