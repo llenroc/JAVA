7766eaa948dc0325c5bed435f9b1f77645734c30
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2021-06-04 12:53:09 -0500

    Update LEETCODE_#53_MaximumSubarray.java

--- ALGORITHM/DATA-STRUCTURES/SOLUTIONS/LEETCODE/DYNAMIC_PROGRAMMING/PATTERN2_KADANE_ALGHORITHM/#53_MAXIMUM_SUBARRAY/LEETCODE_#53_MaximumSubarray.java
+++ ALGORITHM/DATA-STRUCTURES/SOLUTIONS/LEETCODE/DYNAMIC_PROGRAMMING/PATTERN2_KADANE_ALGHORITHM/#53_MAXIMUM_SUBARRAY/LEETCODE_#53_MaximumSubarray.java
@@ -18,7 +18,7 @@ class Solution {
         for(int j=1; j<nums.length; j++)
         {
               currentSum=currentSum+nums[j];
-
+//if a prev sum less than current element in array then move pointer to this element, without further accumulating
             if(currentSum<nums[j])
             { 
                 currentSum=nums[j];  
