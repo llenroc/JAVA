6cf9d1b254a53b939f1d3fe41e6c3b5271e777a5
Oleksii Saiun <44518526+oleksiisaiun@users.noreply.github.com>
2021-06-04 14:02:22 -0500

    Update LEETCODE_#121_BestTimeToBuyAndSellStock.java

--- ALGORITHM/DATA-STRUCTURES/SOLUTIONS/LEETCODE/DYNAMIC_PROGRAMMING/PATTERN2_KADANE_ALGHORITHM/#121_BEST_TIME_TO_BUY_AND_SELL_STOCK/LEETCODE_#121_BestTimeToBuyAndSellStock.java
+++ ALGORITHM/DATA-STRUCTURES/SOLUTIONS/LEETCODE/DYNAMIC_PROGRAMMING/PATTERN2_KADANE_ALGHORITHM/#121_BEST_TIME_TO_BUY_AND_SELL_STOCK/LEETCODE_#121_BestTimeToBuyAndSellStock.java
@@ -4,26 +4,26 @@ LeetCode_#121. Best Time to Buy and Sell Stock
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
 */
 
-
 class Solution {
     public int maxProfit(int[] prices) {
-       Integer profit=0;
-       int buyValue=prices[0];
-       int maxProfit=0;
-       for(int j=1; j<prices.length; j++)
-       {
-           int sellValue=prices[j];
-           int currentDifference=buyValue-sellValue;          
-           if(currentDifference>0)
-           {
-              buyValue = prices[j];
-           }
-           else
-           {
-               maxProfit=Math.max(maxProfit,Math.abs(currentDifference));              
-           }
-       }
-        
-       return maxProfit;
+        int maxProfit=0;
+        int currProfit=0;
+        int sellStockValue=0;
+        int buyStockValue=prices[0];
+            
+            for(int j=1; j<prices.length; j++)
+            {
+                sellStockValue=prices[j];
+                currProfit=sellStockValue-buyStockValue;
+                if(currProfit<0)
+                {
+                    buyStockValue=prices[j];
+                }
+                
+                maxProfit=Math.max(maxProfit,currProfit);
+            }
+            
+            
+            return maxProfit;
     }
 }
